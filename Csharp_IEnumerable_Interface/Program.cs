cacf2afbf83b4201fd9adcfb9eba3e841a490c58
Jama, Najib A <najama@student.rtc.edu>
2019-08-12 05:24:34 -0700

    Initial Commit

---
+++ Csharp_IEnumerable_Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_IEnumerable_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
