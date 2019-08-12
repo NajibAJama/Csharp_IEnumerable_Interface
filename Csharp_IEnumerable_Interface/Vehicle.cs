8c563cabfa626ac709c0e37b4b8aca4de8a15473
Jama, Najib A <najama@student.rtc.edu>
2019-08-12 05:26:30 -0700

    Added Vehicle Class

---
+++ Csharp_IEnumerable_Interface/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp_IEnumerable_Interface
+{
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+        public override string ToString()
+        {
+            return $"Make:{Make} Model:{Model} Year:{Year.Year.ToString()}";
+        }
+    }
+}
